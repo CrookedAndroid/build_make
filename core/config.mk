a000fb15cfa939b5204e89e6fae8ccf54b7456cb
slogar25 <michaelslogar@gmail.com>
2025-04-01 16:47:15 +0000

    turn it crooked

--- core/config.mk
+++ core/config.mk
@@ -459,7 +459,7 @@ endif
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(STATIX_BUILD),)
-include vendor/statix/config/BoardConfigStatix.mk
+include vendor/crooked/config/BoardConfigStatix.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
